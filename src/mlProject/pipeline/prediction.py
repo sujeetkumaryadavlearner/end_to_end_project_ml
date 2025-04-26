01ed244cbf1ff1a8541896f376856f1228b54bd8
sujeetkumaryadavlearner <155717013+sujeetkumaryadavlearner@users.noreply.github.com>
2025-04-26 23:26:40 +0530

    Update prediction.py

--- src/mlProject/pipeline/prediction.py
+++ src/mlProject/pipeline/prediction.py
@@ -2,11 +2,14 @@ import joblib
 import numpy as np
 import pandas as pd
 from pathlib import Path
+import os
 
 
 class PredictionPipeline:
     def __init__(self):
-        self.model=joblib.load(Path('artifact\model_training\model.joblib'))
+        self.model_path = os.path.join('artifact', 'model_training', 'model.joblib')
+        self.model = joblib.load(Path(self.model_path))
+
 
     def predict(self, data):
         data_df = pd.DataFrame(data, columns=["carat", "cut", "color", "clarity", "depth", "table", "x", "y", "z"])
