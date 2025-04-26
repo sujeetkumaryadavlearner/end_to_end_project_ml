4ea0d5617f136dcb79084cc8a5e3b7373693ea8a
Your Name <yadavsujeet2306@gmail.com>
2025-04-26 20:23:18 +0530

    dd

--- app.py
+++ app.py
@@ -17,26 +17,29 @@ def index():
 
 @app.route('/predict', methods=['POST', 'GET'])
 def result():
-    categoried_1={'Fair':0.0, 'Good':1.0, 'Very Good':2.0, 'Premium':3.0, 'Ideal':4.0}
-    clarity_mapping = {'I1':0,'SI2': 1, 'SI1': 2, 'VS2': 3, 'VS1': 4, 'VVS2': 5, 'VVS1': 6, 'IF': 7}
-    color_mapping = {'J': 0, 'I': 1, 'H': 2, 'G': 3, 'F': 4, 'E': 5, 'D': 6}
-    if request.method == 'POST':
-        carat = float(request.form['carat'])
-        cut = request.form['cut']
-        cut= categoried_1[cut]
-        color = request.form['color']
-        color = color_mapping[color]
-        clarity = request.form['clarity']
-        clarity = clarity_mapping[clarity]
-        depth = float(request.form['depth'])
-        table = float(request.form['table'])
-        x = float(request.form['x'])
-        y = float(request.form['y'])
-        z = float(request.form['z'])
-        data=[[carat, cut, color, clarity, depth, table, x, y, z]]
-        prediction=PredictionPipeline()
-        prediction = prediction.predict(data)
-        prediction = np.round(prediction, 4)
-    return render_template('result.html',prediction=prediction[0])
+    try:
+        categoried_1 = {'Fair': 0.0, 'Good': 1.0, 'Very Good': 2.0, 'Premium': 3.0, 'Ideal': 4.0}
+        clarity_mapping = {'I1': 0, 'SI2': 1, 'SI1': 2, 'VS2': 3, 'VS1': 4, 'VVS2': 5, 'VVS1': 6, 'IF': 7}
+        color_mapping = {'J': 0, 'I': 1, 'H': 2, 'G': 3, 'F': 4, 'E': 5, 'D': 6}
+
+        if request.method == 'POST':
+            carat = float(request.form['carat'])
+            cut = categoried_1[request.form['cut']]
+            color = color_mapping[request.form['color']]
+            clarity = clarity_mapping[request.form['clarity']]
+            depth = float(request.form['depth'])
+            table = float(request.form['table'])
+            x = float(request.form['x'])
+            y = float(request.form['y'])
+            z = float(request.form['z'])
+
+            data = [[carat, cut, color, clarity, depth, table, x, y, z]]
+            prediction = PredictionPipeline().predict(data)
+            prediction = np.round(prediction, 4)
+
+            return render_template('result.html', prediction=prediction[0])
+
+    except Exception as e:
+        return f"An error occurred: {e}", 500
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8080)
